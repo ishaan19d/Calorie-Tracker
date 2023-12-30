ce01164a004c248bb30547b6d6e7d8b9d8fd4dac
ishaan19d <ishaandas848@gmail.com>
2023-12-31 01:09:30 +0530

    Initial Commit

---
+++ myapp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render,redirect
+from .models import Food,Consume
+# Create your views here.
+def index(request):
+    if(request.method=="POST"):
+        food_consumed = request.POST['food_consumed']
+        consume=Food.objects.get(name=food_consumed)
+        user=request.user
+        consume=Consume(user=user,food_consumed=consume)
+        consume.save()
+    consumed_food=Consume.objects.filter(user=request.user)
+    foods = Food.objects.all()
+    return render(request,'myapp/index.html',{'foods':foods,'consumed_food':consumed_food})
+
+def delete_consume(request,id):
+    consumed_food=Consume.objects.get(pk=id)
+    if request.method=='POST':
+        consumed_food.delete()
+        return redirect('/')
+    else:
+        return render(request,'myapp/delete.html')
